ca0d792616485c435a7c5d35abbed7f16eef6113
Torito9609 <toroandres85@gmail.com>
2025-05-18 12:17:51 -0500

    prueba git franl

--- src/co/edu/unbosque/controlador/AplMain.java
+++ src/co/edu/unbosque/controlador/AplMain.java
@@ -4,6 +4,7 @@ public class AplMain {
 
 	public static void main(String[] args) {
 		ControladorPrincipal controlador = new ControladorPrincipal();
+		System.out.println("Prueba de git xd");
 
 	}
 
