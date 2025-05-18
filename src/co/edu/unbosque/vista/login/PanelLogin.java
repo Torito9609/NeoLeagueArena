9e0699c4ffcb35c688fffe72e869714faeb10536
Torito9609 <toroandres85@gmail.com>
2025-05-18 11:41:23 -0500

    se crea la vista de login

---
+++ src/co/edu/unbosque/vista/login/PanelLogin.java
@@ -0,0 +1,71 @@
+package co.edu.unbosque.vista.login;
+
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PanelLogin extends JPanel {
+
+    private JTextField correoField;
+    private JPasswordField contrasenaField;
+    private JButton ingresarButton;
+    private JButton cancelarButton;
+
+    public PanelLogin() {
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        setBorder(BorderFactory.createEmptyBorder(20, 40, 20, 40));
+
+        JPanel camposPanel = new JPanel(new GridLayout(2, 2, 10, 10));
+        JLabel correoLabel = new JLabel("Correo:");
+        correoField = new JTextField(20);
+        JLabel contrasenaLabel = new JLabel("Contraseña:");
+        contrasenaField = new JPasswordField(20);
+
+        camposPanel.add(correoLabel);
+        camposPanel.add(correoField);
+        camposPanel.add(contrasenaLabel);
+        camposPanel.add(contrasenaField);
+
+        JPanel botonesPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 15, 10));
+        ingresarButton = new JButton("Ingresar");
+        cancelarButton = new JButton("Cancelar");
+        botonesPanel.add(ingresarButton);
+        botonesPanel.add(cancelarButton);
+
+        add(camposPanel);
+        add(Box.createVerticalStrut(20));
+        add(botonesPanel);
+    }
+
+    public JTextField getCorreoField() {
+        return correoField;
+    }
+
+    public void setCorreoField(JTextField correoField) {
+        this.correoField = correoField;
+    }
+
+    public JPasswordField getContrasenaField() {
+        return contrasenaField;
+    }
+
+    public void setContrasenaField(JPasswordField contrasenaField) {
+        this.contrasenaField = contrasenaField;
+    }
+
+    public JButton getIngresarButton() {
+        return ingresarButton;
+    }
+
+    public void setIngresarButton(JButton ingresarButton) {
+        this.ingresarButton = ingresarButton;
+    }
+
+    public JButton getCancelarButton() {
+        return cancelarButton;
+    }
+
+    public void setCancelarButton(JButton cancelarButton) {
+        this.cancelarButton = cancelarButton;
+    }
+}
---
+++ src/co/edu/unbosque/vista/login/VentanaLogin.java
@@ -0,0 +1,28 @@
+package co.edu.unbosque.vista.login;
+
+import javax.swing.*;
+
+public class VentanaLogin extends JFrame {
+
+    private PanelLogin panelLogin;
+
+    public VentanaLogin() {
+        setTitle("Iniciar Sesión");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 200);
+        setLocationRelativeTo(null); 
+        setResizable(false);
+
+        panelLogin = new PanelLogin();
+        add(panelLogin);
+    }
+
+    public PanelLogin getPanelLogin() {
+        return panelLogin;
+    }
+
+    public void setPanelLogin(PanelLogin panelLogin) {
+        this.panelLogin = panelLogin;
+    }
+}
+
